82bc5ab944fa8fb1fe2f655a20446a1fbe88d93b
jofrey.onesmo <jofreyjofreyonesmo@gmail.com>
2025-03-18 18:32:28 +0300

    prod changed

--- src/main/java/com/jeff/db_connector/HomeController.java
+++ src/main/java/com/jeff/db_connector/HomeController.java
@@ -1,9 +1,12 @@
 package com.jeff.db_connector;
 
+import com.jeff.db_connector.dto.CustomerRequest;
+import com.jeff.db_connector.model.Customer;
 import com.jeff.db_connector.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,5 +32,17 @@ public class HomeController {
     @GetMapping("/customers/active")
     public ResponseEntity<?> getActiveCustomers() {
         return ResponseEntity.ok(customerRepository.findAllByStatus("A"));
+    }
+
+    @GetMapping("/customers/create")
+    public ResponseEntity<?> createCustomer(@RequestBody CustomerRequest request) {
+        Customer newCustomer = new Customer();
+        newCustomer.setFirstname(request.getFirstname());
+        newCustomer.setMiddlename(request.getMiddlename());
+        newCustomer.setAccountNo(request.getAccountNo());
+        newCustomer.setStatus("A");
+
+        Customer savedCustomer = customerRepository.save(newCustomer);
+        return ResponseEntity.ok(savedCustomer);
     }
 }
---
+++ src/main/java/com/jeff/db_connector/dto/CustomerRequest.java
@@ -0,0 +1,33 @@
+package com.jeff.db_connector.dto;
+
+import jakarta.persistence.Column;
+
+public class CustomerRequest {
+    private String firstname;
+    private String middlename;
+    private String accountNo;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+}
